97d5ac7fc4fb3270848b3082db9300162b99eae7
Ioanna Papad <ioannapapad04@gmail.com>
2024-11-11 15:28:48 +0200

    template

---
+++ GeneticAlgorithm/GeneticAlgorithm/Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        GeneticAlgorithm algorithm = new GeneticAlgorithm();
+        //populationSize, mutationProbability, maximumSteps, minimumFitness
+        Chromosome solution = algorithm.run(1000, 0.08, 1000, 28);
+        solution.print();
+    }
+}
