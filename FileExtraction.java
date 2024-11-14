a5ed66b4c9c1ff710fe01d8812a10f20829f505c
Joanna Papadakaki <148077133+Joannapapad@users.noreply.github.com>
2024-11-14 16:28:12 +0200

    Update FileExtraction.java

--- FileExtraction.java
+++ FileExtraction.java
@@ -1,13 +1,25 @@
 import java.io.*;
 
+<<<<<<< HEAD
 public class FileExtraction{
 
     public int ReadLines(String filePath) throws IOException{
+=======
+public class FileExtraction {
+    private Lesson[] lessons;  // Store lessons in a class-level variable
+    private Teacher[] teachers; // Store teachers in a class-level variable
+
+    // Method to read the number of lines in the file
+    public int ReadLines(String filePath) throws IOException {
+>>>>>>> 3c1b638 (genes)
         String line = "";
         File file = new File(filePath);
         BufferedReader br = new BufferedReader(new FileReader(file));
         int count = 0;
+<<<<<<< HEAD
         int i = 0;
+=======
+>>>>>>> 3c1b638 (genes)
         while ((line = br.readLine()) != null) {
             count++;
         }
@@ -15,6 +27,7 @@ public class FileExtraction{
         return count;
     }
 
+<<<<<<< HEAD
     public void ReadLessons (String filePath ) throws IOException {
         String line = "";
         File file = new File(filePath);
@@ -56,3 +69,49 @@ public class FileExtraction{
 
 
 
+=======
+    // Method to read lessons from the file and store them in the lessons array
+    public void ReadLessons(String filePath) throws IOException {
+        String line = "";
+        File file = new File(filePath);
+        int count = ReadLines(filePath);  // Get the number of lessons
+        lessons = new Lesson[count];  // Initialize the lessons array
+        BufferedReader br2 = new BufferedReader(new FileReader(file));
+        
+        int i = 0;
+        while ((line = br2.readLine()) != null) {
+            String[] temp = line.split("\\s+");
+            Lesson t = new Lesson(Integer.parseInt(temp[0]), temp[1], temp[2].charAt(0), Integer.parseInt(temp[3]));
+            lessons[i] = t;
+            i++;
+        }
+    }
+
+    // Method to read teachers from the file and store them in the teachers array
+    public void ReadTeachers(String filePath) throws IOException {
+        String line = "";
+        File file = new File(filePath);
+        int count = ReadLines(filePath);  // Get the number of teachers
+        teachers = new Teacher[count];  // Initialize the teachers array
+        BufferedReader br2 = new BufferedReader(new FileReader(file));
+        
+        int i = 0;
+        while ((line = br2.readLine()) != null) {
+            String[] temp = line.split("\\s+");
+            Teacher t = new Teacher(Integer.parseInt(temp[0]), temp[1], temp[2], Integer.parseInt(temp[3]), Integer.parseInt(temp[4]));
+            teachers[i] = t;
+            i++;
+        }
+    }
+
+    // Getter for the lessons array
+    public Lesson[] getLessons() {
+        return lessons;
+    }
+
+    // Getter for the teachers array
+    public Teacher[] getTeachers() {
+        return teachers;
+    }
+}
+>>>>>>> 3c1b638 (genes)
